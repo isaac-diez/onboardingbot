fa1a2406ad7458dbc60cf3ce1410d63beb3dba06
isaac diez <isaac.diez@proton.me>
2025-10-02 21:04:16 +0200

    create: app for project and tests

---
+++ src/main/java/com/hackaton/onboardingbot/OnboardingBotApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.onboardingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton/onboardingbot/OnboardingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.onboardingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
