49c778dee5ff9ab3588928a188a17a9b9100188b
isaac diez <isaac.diez@proton.me>
2025-10-03 19:28:24 +0200

    refactor: service to work with dtos successfully

--- src/main/java/com/hackaton/onboardingbot/service/KnowledgeService.java
+++ src/main/java/com/hackaton/onboardingbot/service/KnowledgeService.java
@@ -1,15 +1,15 @@
 package com.hackaton.onboardingbot.service;
 
+import com.hackaton.onboardingbot.model.KnowledgeCreateDTO;
+import com.hackaton.onboardingbot.model.KnowledgeDTO;
 import com.hackaton.onboardingbot.model.KnowledgeEntry;
-import com.hackaton.onboardingbot.repository.KnowledgeRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public interface KnowledgeService {
 
-    List<KnowledgeEntry> getAllEntries();
-    KnowledgeEntry createKnowledgeEntry(KnowledgeEntry knowledgeEntry);
+    List<KnowledgeDTO> getAllEntries();
+    KnowledgeEntry createKnowledgeEntry(KnowledgeCreateDTO newEntryDto);
 
 
 }
--- src/main/java/com/hackaton/onboardingbot/service/KnowledgeServiceImpl.java
+++ src/main/java/com/hackaton/onboardingbot/service/KnowledgeServiceImpl.java
@@ -1,9 +1,10 @@
 package com.hackaton.onboardingbot.service;
 
 import com.hackaton.onboardingbot.model.KnowledgeCreateDTO;
+import com.hackaton.onboardingbot.model.KnowledgeDTO;
 import com.hackaton.onboardingbot.model.KnowledgeEntry;
+import com.hackaton.onboardingbot.model.KnowledgeMapper;
 import com.hackaton.onboardingbot.repository.KnowledgeRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,18 +13,24 @@ import java.util.List;
 public class KnowledgeServiceImpl implements KnowledgeService{
 
     private KnowledgeRepo knowledgeRepo;
+    private KnowledgeMapper mapper;
 
-    public KnowledgeServiceImpl(KnowledgeRepo knowledgeRepo) {
+    public KnowledgeServiceImpl(KnowledgeRepo knowledgeRepo, KnowledgeMapper mapper) {
         this.knowledgeRepo = knowledgeRepo;
+        this.mapper = mapper;
     }
 
     @Override
-    public List<KnowledgeEntry> getAllEntries() {
-        return knowledgeRepo.findAll();
+    public List<KnowledgeDTO> getAllEntries() {
+        return knowledgeRepo.findAll()
+                .stream()
+                .map(mapper::toDto)
+                .toList();
     }
 
     @Override
-    public KnowledgeEntry createKnowledgeEntry(KnowledgeEntry knowledgeEntry) {
-        return knowledgeRepo.save(knowledgeEntry);
+    public KnowledgeEntry createKnowledgeEntry(KnowledgeCreateDTO queryDTO) {
+        KnowledgeEntry query = mapper.toQuery(queryDTO);
+        return knowledgeRepo.save(query);
     }
 }
