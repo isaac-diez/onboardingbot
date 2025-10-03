55a17867b90f4a1fefd39977c30653801715dea1
isaac diez <isaac.diez@proton.me>
2025-10-03 15:57:25 +0200

    feat: created service

--- src/main/java/com/hackaton/onboardingbot/service/KnowledgeServiceImpl.java
+++ src/main/java/com/hackaton/onboardingbot/service/KnowledgeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hackaton.onboardingbot.service;
 
+import com.hackaton.onboardingbot.model.KnowledgeCreateDTO;
 import com.hackaton.onboardingbot.model.KnowledgeEntry;
 import com.hackaton.onboardingbot.repository.KnowledgeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +11,19 @@ import java.util.List;
 @Service
 public class KnowledgeServiceImpl implements KnowledgeService{
 
-    @Autowired
-    KnowledgeRepo knowledgeRepo;
+    private KnowledgeRepo knowledgeRepo;
+
+    public KnowledgeServiceImpl(KnowledgeRepo knowledgeRepo) {
+        this.knowledgeRepo = knowledgeRepo;
+    }
 
     @Override
     public List<KnowledgeEntry> getAllEntries() {
-        return List.of();
+        return knowledgeRepo.findAll();
     }
 
     @Override
     public KnowledgeEntry createKnowledgeEntry(KnowledgeEntry knowledgeEntry) {
-        return null;
+        return knowledgeRepo.save(knowledgeEntry);
     }
 }
