33f3c4522d84d27a82eaa1c8c8edd5295debe84b
isaac diez <isaac.diez@proton.me>
2025-10-04 20:26:51 +0200

    update: added constructor to KnowledgeCreateDTO

--- src/main/java/com/hackaton/onboardingbot/model/KnowledgeCreateDTO.java
+++ src/main/java/com/hackaton/onboardingbot/model/KnowledgeCreateDTO.java
@@ -1,8 +1,10 @@
 package com.hackaton.onboardingbot.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class KnowledgeCreateDTO {
     private String question;
     private String answer;
