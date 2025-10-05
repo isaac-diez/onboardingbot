f3e29bbabbd4a6060072fb220c0c4fe4d0185414
isaac diez <isaac.diez@proton.me>
2025-10-05 18:29:39 +0200

    update: added findByWeywordIn()

--- src/main/java/com/hackaton/onboardingbot/repository/KnowledgeRepo.java
+++ src/main/java/com/hackaton/onboardingbot/repository/KnowledgeRepo.java
@@ -3,5 +3,8 @@ package com.hackaton.onboardingbot.repository;
 import com.hackaton.onboardingbot.model.KnowledgeEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface KnowledgeRepo extends JpaRepository <KnowledgeEntry, Integer>{
+    List<KnowledgeEntry> findByKeywordIn(List<String> keywords);
 }
