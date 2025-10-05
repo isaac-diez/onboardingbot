4161b3b519504faa1f9eeca5a281bd05cf2b7b47
isaac diez <isaac.diez@proton.me>
2025-10-05 18:29:16 +0200

    feat: added knowledge/question endpoint

--- src/main/java/com/hackaton/onboardingbot/controller/KnowledgeController.java
+++ src/main/java/com/hackaton/onboardingbot/controller/KnowledgeController.java
@@ -4,6 +4,7 @@ import com.hackaton.onboardingbot.model.KnowledgeCreateDTO;
 import com.hackaton.onboardingbot.model.KnowledgeDTO;
 import com.hackaton.onboardingbot.model.KnowledgeEntry;
 import com.hackaton.onboardingbot.service.KnowledgeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +22,11 @@ public class KnowledgeController {
     @GetMapping("/knowledge")
     public List<KnowledgeDTO> getEntries() {
         return knowledgeService.getAllEntries();
+    }
+
+    @GetMapping("/knowledge/question")
+    public List<KnowledgeDTO> getEntryByQuestion(@RequestBody String question) {
+        return knowledgeService.searchByQuestionKeywords(question);
     }
 
     @PostMapping("/knowledge")
